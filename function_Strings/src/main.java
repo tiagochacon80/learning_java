a9c08ba444abacc30024193cbd91968c9bf984ed
Tiago Chacon <tiagochacon80@gmail.com>
2022-04-07 15:17:50 -0400

    functions Strings

---
+++ function_Strings/src/main.java
@@ -0,0 +1,14 @@
+
+public class main {
+
+	public static void main(String[] args) {
+		
+		String original = "abcde FGHIJ ABC abc DEFG";
+		
+		String s01 = original.toLowerCase();
+		
+		System.out.println(s01);
+
+	}
+
+}
