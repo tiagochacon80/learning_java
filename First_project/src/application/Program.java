448686b1e6b440aa3017b4d5e0c311b752cb0dc4
Tiago Chacon <tiagochacon80@gmail.com>
2022-04-08 12:04:51 -0400

    First project - inventory control

--- First_project/src/application/Program.java
+++ First_project/src/application/Program.java
@@ -1,9 +1,29 @@
 package application;
 
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Product;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		Product product = new Product();
+		System.out.println("Enter product data: ");
+		System.out.print("Name: ");
+		product.name = sc.nextLine();
+		System.out.print("Price: ");
+		product.price = sc.nextDouble();
+		System.out.println("Quantity in stock: ");
+		product.quantity = sc.nextInt();
+		
+		System.out.println(product.name + product.price + product.quantity);
+		
+		sc.close();
 
 	}
 
--- First_project/src/entities/Product.java
+++ First_project/src/entities/Product.java
@@ -1,5 +1,21 @@
 package entities;
 
 public class Product {
-
+	
+	public String name;
+	public double price;
+	public int quantity;
+	
+	public double totalValueInStock() {
+		return price * quantity;
+	}
+	
+	//Ajoute le produit dans l'inventaire
+	public void addProducts(int quantity) {
+		this.quantity += quantity;
+	}
+	//remove le produit dans l'inventaire
+	public void removeProducts(int quantity) {
+		this.quantity -= quantity;
+	}
 }
