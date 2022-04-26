dd334eb2a00048a28c9f5b70456ae95a4a29b8c9
Tiago Chacon <tiagochacon80@gmail.com>
2022-04-26 10:35:28 -0400

    Orienté objet, getters et setters

---
+++ Orientée_objet/src/getters_et_setters/cours04.java
@@ -0,0 +1,12 @@
+package getters_et_setters;
+
+public class cours04 {
+
+	public static void main(String[] args) {
+		Caneta c1 = new Caneta("NIC", "Amarela", 0.4f);		
+		c1.status();
+		Caneta c2 = new Caneta("Poly", "Vermelha", 1.7f);
+		c2.status();
+	}
+
+}
---
+++ Orientée_objet/src/règles_de_visibilité/Cours02.java
@@ -0,0 +1,17 @@
+package règles_de_visibilité;
+
+public class Cours02 {
+
+	public static void main(String[] args) {
+		Caneta c1 = new Caneta();
+		c1.modelo = "BIC cristal";
+		c1.cor = "Azul";
+		//c1.ponta = 0.5f;
+		c1.carga = 80;
+		//c1.tampada = false;
+		c1.destampar();
+		c1.status();
+		c1.rabiscar();
+	}
+
+}
