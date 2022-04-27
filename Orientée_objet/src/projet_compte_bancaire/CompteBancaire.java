87edf55a8473ccffe850856b10141f7ce82d2e7c
Tiago Chacon <tiagochacon80@gmail.com>
2022-04-27 14:09:39 -0400

    projet compte bancaire

--- Orientée_objet/src/projet_compte_bancaire/CompteBancaire.java
+++ Orientée_objet/src/projet_compte_bancaire/CompteBancaire.java
@@ -8,20 +8,69 @@ public class CompteBancaire {
 	private float solde;
 	private boolean status;	
 	//Méthodes
-	public void ouvrirCompte() {
+	public void etatActuel() {
+		System.out.println("----------------------------------");
+		System.out.println("Compte: " + this.getNumCompte());
+		System.out.println("Type: " + this.getType());
+		System.out.println("Nom au complet: " + this.getProprietaire());
+		System.out.println("Solde: " + this.getSolde());
+		System.out.println("Status: " + this.getStatus());
+	}
+	
+	public void ouvrirCompte(String t) {
+		this.setType(t);
+		this.setStatus(true);
+		if (t == "CC") {
+			this.setSolde(50);
+		}else if (t == "CP") {
+			this.setSolde(150);
+		}
+		System.out.println("Le compte a été ouvert avec succès");
 		
 	}
 	public void fermerCompte() {
-		
+		if (this.getSolde() > 0) {
+			System.out.println("Le compte ne peut pas être fermer, car le solde n'est pas zéro");
+		}else if (this.getSolde() < 0) {
+			System.out.println("Le compte ne peut pas être fermer, car il a une dette");
+		}else {
+			this.setStatus(false);
+			System.out.println("Compte fermé");
+		}
 	}
-	public void depot() {
-		
+	public void depot(float valeur) {
+		if (this.getStatus()){
+			this.setSolde(this.getSolde() + valeur);
+			System.out.println("Le dépôt a été réalisé avec succès " + this.getProprietaire());
+		}else {
+			System.out.println("Imposssible faire un dépôt, le compte est fermé");
+		}
 	}
-	public void retirerArgent() {
-		
+	public void retirerArgent(float valeur) {
+		if (this.getStatus()) {
+			if (this.getSolde() >= valeur) {
+				this.setSolde(this.getSolde() - valeur);
+				System.out.println("Retrait effectué sur le compte " + this.getProprietaire());
+			}else {
+				System.out.println("fonds insuffisants");
+			}
+		}else {
+			System.out.println("Impossible de retirer d'argent d'un compte fermé!");
+		}
 	}
-	public void payerForfait() {
-		
+	public void fraisMensuel() {
+		int valeur = 0;
+		if (this.getType() == "CC") {
+			valeur = 12;
+		}else if (this.getType() == "CP") {
+			valeur = 20;
+		}
+		if (this.getStatus()) {
+			this.setSolde(this.getSolde() - valeur);
+			System.out.println("Frais mensuels payés");
+		}else {
+			System.out.println("Impossible de payer, car le compte est fermé!");
+		}
 	}
 	//Méthode spéciaux
 	public CompteBancaire() {
--- Orientée_objet/src/projet_compte_bancaire/Main.java
+++ Orientée_objet/src/projet_compte_bancaire/Main.java
@@ -3,8 +3,25 @@ package projet_compte_bancaire;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		CompteBancaire client1 = new CompteBancaire();
+		client1.setNumCompte(1668);
+		client1.setProprietaire("Tiago Celani Chacon");
+		client1.ouvrirCompte("CC");
+		
+		CompteBancaire client2 = new CompteBancaire();
+		client2.setNumCompte(1774);
+		client2.setProprietaire("Andreza Borba");
+		client2.ouvrirCompte("CP");
+		
+		client1.depot(100);
+		client2.depot(500);
+		client2.retirerArgent(1000);
+		
+		client1.retirerArgent(150);
+		client1.fermerCompte();
+		
+		client1.etatActuel();
+		client2.etatActuel();
 	}
 
 }
