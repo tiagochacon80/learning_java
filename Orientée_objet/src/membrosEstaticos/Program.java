e0af96f22ca9be5d5fd09754272942355f96c391
Tiago Chacon <tiagochacon80@gmail.com>
2022-05-04 13:46:35 -0400

    méthode static

--- Orientée_objet/src/membrosEstaticos/Program.java
+++ Orientée_objet/src/membrosEstaticos/Program.java
@@ -3,26 +3,23 @@ package membrosEstaticos;
 import java.util.Locale;
 import java.util.Scanner;
 
-import membrosEstaticos.Calculator;
-
 public class Program {	
 
 	public static void main(String[] args) {
 		
 		Locale.setDefault(Locale.US);
-		Scanner sc = new Scanner(System.in);
-		Calculator calc = new Calculator();
+		Scanner sc = new Scanner(System.in);		
 		
 		System.out.print("Enter radius: ");
 		double radius = sc.nextDouble();
 		
-		double c = calc.circumference(radius);
+		double c = Calculator.circumference(radius);
 		
-		double v = calc.volume(radius);	
+		double v = Calculator.volume(radius);	
 		
 		System.out.printf("Circumference: %.2f%n", c);
 		System.out.printf("Volume: %.2f%n", v);
-		System.out.printf("PI value: %.2f%n", calc.PI);
+		System.out.printf("PI value: %.2f%n", Calculator.PI);
 		
 		sc.close();
 	}
