4353fdf30ccedd76f44d676a8ed43f8f8fd4490b
Tiago Chacon <tiagochacon80@gmail.com>
2022-05-10 15:09:29 -0400

    ArrayList

--- ArrayList/src/application/Program.java
+++ ArrayList/src/application/Program.java
@@ -2,14 +2,13 @@ package application;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		//Declarando como inteira (wrapper class)
-		//List<Integer> list;
-		//Instanciando uma lista com uma classe que implementa a interface
+		
 		List<String> list = new ArrayList<>(); 
 		
 		list.add("Volvo");
@@ -17,10 +16,31 @@ public class Program {
 		list.add("Ford");
 		list.add("Mazda");
 		
+		list.add(2, "Hyundai");
+		System.out.println(list.size());
+		
+		
 		for (String i : list) {
 			System.out.println(i);
 		}
-
+		System.out.println("--------");						
+		list.remove(1);
+		
+		list.removeIf(x -> x.charAt(0) == 'M');
+		for(String i : list) {
+			System.out.println(i);
+		}
+		System.out.println("--------");		
+		System.out.println("Index of BMW: " + list.indexOf("BMW"));
+		
+		System.out.println("--------");		
+		List<String> result = list.stream().filter(x -> x.charAt(0) == 'H').collect(Collectors.toList());
+		for (String i : result) {
+			System.out.println(i);
+		}
+		
+		System.out.println("--------");		
+		String name = list.stream().filter(x -> x.charAt(0) == 'F').findFirst().orElse(null); 
+		System.out.println(name);
 	}
-
 }
