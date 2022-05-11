12d45a152779824f13171f5ec3452fa9c5c8a858
Tiago Chacon <tiagochacon80@gmail.com>
2022-05-11 14:51:20 -0400

    project employee

--- list-exercice/src/aplication/Program.java
+++ list-exercice/src/aplication/Program.java
@@ -13,15 +13,36 @@ public class Program {
 		
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-		
+				
+		//Criando um List(generiques) do tipo Employee
 		List<Employee> list = new ArrayList<>();
 		
-		System.out.println("How many employees will be registered? ");
-		int n = sc.nextInt();
+		//Part 1 - Reading data
+		
+		System.out.print("How many employees will be registered? ");
+		int N = sc.nextInt();
+		
+		for (int i=0; i<N; i++) {
+			System.out.println();
+			System.out.println("Employee #" + (i+1));
+			System.out.print("Id: ");
+			int id = sc.nextInt();
+			System.out.print("Name: ");
+			sc.nextLine();
+			String name = sc.nextLine();
+			System.out.print("Salary: ");
+			Double salary = sc.nextDouble();
+			
+			//Instanciando o objeto
+			Employee emp = new Employee(id, name, salary);
+			
+			list.add(emp);
+		}
 		
 		
 		
 		
+		sc.close();		
 
 	}
 
