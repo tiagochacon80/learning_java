47ea0e8f95212691ee1726250baf712d49dbdcf9
Tiago Chacon <tiagochacon80@gmail.com>
2022-05-13 15:24:02 -0400

    exemple de array multi-dimensional

---
+++ Multi-Dimenscional-Arrays/src/Exemple/Main.java
@@ -0,0 +1,25 @@
+package Exemple;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		double[][] noteEleves = new double[30][4];
+		
+		noteEleves[0][0] = 10;
+		noteEleves[0][1] = 7;
+		noteEleves[0][2] = 9;
+		noteEleves[0][3] = 9.5;
+		
+		noteEleves[1][0] = 9;
+		noteEleves[1][1] = 8;
+		noteEleves[1][2] = 7;
+		noteEleves[1][3] = 9;
+		
+		noteEleves[2][0] = 9;
+		noteEleves[2][1] = 8;
+		noteEleves[2][2] = 7;
+		noteEleves[2][3] = 9;
+	}
+
+}
