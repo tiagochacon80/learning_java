cde3c77e5c18fb65d52f43bf3b4033c3ad9e0c12
Tiago Chacon <tiagochacon80@gmail.com>
2022-05-16 10:34:19 -0400

    exemple de array multi-dimensional

---
+++ Multi-Dimenscional-Arrays/src/Exemple/ArrayMultidimensional.java
@@ -0,0 +1,41 @@
+package Exemple;
+
+public class ArrayMultidimensional {
+
+	public static void main(String[] args) {
+		
+		int[][][] arrayTri = new int[3][3][3]; 
+		
+		for(int i=0; i<arrayTri.length; i++) {
+			for(int j=0; j<arrayTri[i].length; j++) {
+				for(int k=0; k<arrayTri[i][j].length; k++) {
+					System.out.println("i = " + i + " j = " + j + " k = " + k);
+					arrayTri[i][j][k] = i + j + k;
+				}
+			}
+		}
+		
+		int somme = 0;
+		int sommePaires = 0;
+		int sommeImpair = 0;
+		for(int i=0; i<arrayTri.length; i++) {
+			for(int j=0; j<arrayTri[i].length; j++) {
+				for(int k=0; k<arrayTri[i][j].length; k++) {
+					
+					somme += arrayTri[i][j][k];
+					
+					if(arrayTri[i][j][k] % 2 == 0) {
+						sommePaires += arrayTri[i][j][k];
+					} else {
+						sommeImpair += arrayTri[i][j][k];
+					}
+				}
+			}
+		}
+		System.out.println("Somme total = " + somme);
+		System.out.println("Somme paire = " + sommePaires);
+		System.out.println("Somme impaire = " + sommeImpair);
+
+	}
+
+}
