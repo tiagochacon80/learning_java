a2d0702802dc1aa2e19bad238fe40199c28f4a99
Tiago Chacon <tiagochacon80@gmail.com>
2022-05-27 11:29:29 -0400

    Polymorphism Override

---
+++ Polymorphism/src/projetoanimal/Reptil.java
@@ -0,0 +1,24 @@
+package projetoanimal;
+
+public class Reptil extends Animal{
+	private String corEscama;
+	
+	@Override
+	public void locomover() {
+		System.out.println("Rastejando");		
+	}
+
+	@Override
+	public void alimentar() {
+		System.out.println("Comendo vegetais");		
+	}
+
+	@Override
+	public void emitirSom() {
+		System.out.println("Som de réptil");		
+	}
+
+	
+	
+	
+}
