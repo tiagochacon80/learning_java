da64587a1d1459060d2b5652a5889f87c90f6f83
Tiago Chacon <tiagochacon80@gmail.com>
2022-05-31 10:27:33 -0400

    Encapsulation project control

---
+++ Encapsulation/src/projetoControlador/ControleRemoto.java
@@ -0,0 +1,38 @@
+package projetoControlador;
+
+public class ControleRemoto {
+	private int volume;
+	private boolean ligado;
+	private boolean tocando;
+	
+	public ControleRemoto() {
+		this.volume = 50;
+		this.ligado = false;
+		this.tocando = false;
+	}
+
+	public int getVolume() {
+		return volume;
+	}
+
+	public void setVolume(int volume) {
+		this.volume = volume;
+	}
+
+	public boolean isLigado() {
+		return ligado;
+	}
+
+	public void setLigado(boolean ligado) {
+		this.ligado = ligado;
+	}
+
+	public boolean isTocando() {
+		return tocando;
+	}
+
+	public void setTocando(boolean tocando) {
+		this.tocando = tocando;
+	}	
+	
+}
