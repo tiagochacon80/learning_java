8831402c426e6273f9e65551172128eff926f012
Tiago Chacon <tiagochacon80@gmail.com>
2022-06-07 16:04:18 -0400

    project shape inheritance

--- Inheritance/src/projectFigures/Program.java
+++ Inheritance/src/projectFigures/Program.java
@@ -1,5 +1,7 @@
 package projectFigures;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -10,8 +12,34 @@ public class Program {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
+		List<Shape> list = new ArrayList<>();
 		
+		System.out.print("Enter the number of shapes: ");
+		int n = sc.nextInt();
+		
+		for(int i=1; i<=n; i++) {
+			System.out.println("Shape #" + i + "data:");
+			System.out.print("Rectangle or Circle (r/c): ");
+			char type = sc.next().charAt(0);
+			System.out.print("Color (BLACK/ BLUE/RED): ");
+			Color color = Color.valueOf(sc.next());	
+			if(type == 'r') {
+				System.out.print("Width: ");
+				double width = sc.nextDouble();
+				System.out.println("Height: ");
+				double height = sc.nextDouble();
+				list.add(new Rectangle(color, width, height));
+			} else {
+				System.out.print("Radius: ");
+				double radius = sc.nextDouble();
+				list.add(new Circle(color, radius));
+			}			
+		}		
+		System.out.println();
+		System.out.println("SHAPE AREAS:");
+		for(Shape shape : list) {
+			System.out.println(String.format("%.2f", shape.area();
+		}
 		sc.close();
 	}
-
 }
