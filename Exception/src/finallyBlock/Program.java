bfd61e3f88f94ce5861b428890d8fcd49e9f5f5c
Tiago Chacon <tiagochacon80@gmail.com>
2022-06-09 10:18:38 -0400

    Block finally

---
+++ Exception/src/finallyBlock/Program.java
@@ -0,0 +1,30 @@
+package finallyBlock;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		File file = new File("C:\\Users\\tiago\\Documents\\Curso Java - Nelio dias\\ws-eclipse\\Exception\\int.txt");
+		Scanner sc = null;
+		try {
+			sc = new Scanner(file);
+			while (sc.hasNextLine()) {
+				System.out.println(sc.nextLine());
+			}
+		}
+		catch (IOException e) {
+			System.out.println("Error opening file: " + e.getMessage());
+		}
+		finally {
+			if(sc != null) {
+				sc.close();
+			}
+			System.out.println("Finally block executed");
+		}
+	}
+
+}
