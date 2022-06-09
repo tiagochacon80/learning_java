97cf4f3c30e2388e2eda79e7689920cd1cbcb291
Tiago Chacon <tiagochacon80@gmail.com>
2022-06-09 09:12:39 -0400

    Exception

---
+++ Exception/src/example/Program.java
@@ -0,0 +1,30 @@
+package example;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		try {
+			String[] vect = sc.nextLine().split(" ");
+			int position = sc.nextInt();
+			System.out.println(vect[position]);
+		}
+		//Posiçao nao existente no array
+		catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println("Invalid position");
+		}
+		catch (InputMismatchException e) {
+			System.out.println("Input error");
+		}
+		
+		System.out.println("End of program");
+
+	sc.close();
+	
+	}
+
+}
