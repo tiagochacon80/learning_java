6fed4225bcb110adac7607fc110eef372ef5b539
Tiago Chacon <tiagochacon80@gmail.com>
2022-06-16 09:59:56 -0400

    OO toString, Class Program

--- Orientée_objet/src/projetStock/Product.java
+++ Orientée_objet/src/projetStock/Product.java
@@ -7,15 +7,26 @@ public class Product {
 	public int quantity;
 	
 	
-	public void addProducts(int quantity) {
-		quantity += quantity;
-	}
-	
-	public void removeProducts(int quantity) {
-		quantity -= quantity;
-	}
-	
 	public double totalValueInStock() {
 		return price * quantity;
 	}
+	
+	public void addProducts(int quantity) {
+		this.quantity += quantity;
+	}
+	
+	public void removeProducts(int quantity) {
+		this.quantity -= quantity;
+	}
+	
+	public String toString() {
+		return name 
+			+ ", $ "
+			+ String.format("%.2f", price)
+			+ ", "
+			+ quantity
+			+ " units, Total: $ "
+			+ String.format("%.2f", totalValueInStock());
+	}
+	
 }
--- Orientée_objet/src/projetStock/Program.java
+++ Orientée_objet/src/projetStock/Program.java
@@ -1,10 +1,42 @@
 package projetStock;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		Product product = new Product();
+		
+		System.out.println("Enter product data:");
+		System.out.print("Name: ");
+		product.name = sc.nextLine();
+		System.out.print("Price: ");
+		product.price = sc.nextDouble();
+		System.out.print("Quantity in stock: ");
+		product.quantity = sc.nextInt();		
+		
+		System.out.println();
+		System.out.println("Product data: " + product);
+		
+		System.out.println();
+		System.out.print("Enter the number of products to be added in stock: ");
+		int quantity = sc.nextInt();
+		product.addProducts(quantity);
+		
+		System.out.println("Upadate data: " + product);
+		
+		System.out.println();
+		System.out.print("Enter the number of products to be removed in stock: ");
+		quantity = sc.nextInt();
+		product.removeProducts(quantity);
+		
+		System.out.println("Update data: " + product);		
+		
+		sc.close();
 	}
-
 }
