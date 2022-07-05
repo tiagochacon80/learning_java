e493e869e6e05e17872eafd25247624703377c46
Tiago Chacon <tiagochacon80@gmail.com>
2022-07-05 15:05:03 -0400

    Try with resources

---
+++ File/src/tryWithResources/Program.java
@@ -0,0 +1,26 @@
+package tryWithResources;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Program {
+	
+	public static void main(String[] args) {
+
+		String path =  "c:\\temp\\ina.txt";
+		
+		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+			String line = br.readLine();
+			
+			while (line != null) {
+				System.out.println(line);
+				line = br.readLine();
+			}
+		}
+		catch (IOException e) {
+			System.out.println("Error: " + e.getMessage());
+		}
+	}
+}
