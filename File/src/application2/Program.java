b8f9eb748b0701ac0907284d05b3e8abab5b7d8e
Tiago Chacon <tiagochacon80@gmail.com>
2022-07-07 09:34:07 -0400

    Project Interface - Car Rental

--- File/src/application2/Program.java
+++ File/src/application2/Program.java
@@ -1,10 +1,25 @@
 package application2;
 
+//Proposed path information
+
+import java.io.File;
+import java.util.Scanner;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter a folder path: ");
+		String strPath = sc.nextLine();
+		
+		File path = new File(strPath);
+		
+		System.out.println("getPath: " + path.getPath());
+		System.out.println("getParent: " + path.getParent());
+		System.out.println("getName: " + path.getName());
+		
+		sc.close();
 	}
-
 }
