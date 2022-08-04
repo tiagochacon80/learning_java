5a4e1c2aaa392b6baffbaa8fe521cee8af9e0a3b
Tiago Chacon <tiagochacon80@gmail.com>
2022-08-04 11:34:35 -0400

    CanadaInterestService class

--- interfaces3/src/entities/CanadaInterestService.java
+++ interfaces3/src/entities/CanadaInterestService.java
@@ -1,2 +1,27 @@
-package entities;public class CanadaInterestService {
+package entities;
+
+import java.security.InvalidParameterException;
+
+public class CanadaInterestService {
+
+    private double interestRate;
+
+    public CanadaInterestService(double interestRate) {
+        this.interestRate = interestRate;
+    }
+
+    public double getInterestRate() {
+        return interestRate;
+    }
+
+    public void setInterestRate(double interestRate) {
+        this.interestRate = interestRate;
+    }
+
+    public double payment(double amount, int months) {
+        if (months < 1) {
+            throw new InvalidParameterException("Months must be greater than zero");
+        }
+        return amount * Math.pow(1.0 + interestRate / 100, months);
+    }
 }
