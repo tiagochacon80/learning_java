1f72a3cde744ee0db6c93fdf0897f96836ccfcab
Tiago Chacon <tiagochacon80@gmail.com>
2022-08-04 11:45:47 -0400

    Program class

---
+++ interfaces3/src/application/Program.java
@@ -0,0 +1,28 @@
+package application;
+
+import entities.CanadaInterestService;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Amount: ");
+        double amount = sc.nextDouble();
+        System.out.print("Months: ");
+        int months = sc.nextInt();
+
+        CanadaInterestService cis = new CanadaInterestService(2.0);
+        double payment = cis.payment(amount, months);
+
+        System.out.println("Payment after " + months + " months:");
+        System.out.println(String.format("%.2f", payment));
+
+        sc.close();
+    }
+}
