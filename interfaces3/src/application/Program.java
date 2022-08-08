72de0b2a7d24993b1c772baa725002112074da57
Tiago Chacon <tiagochacon80@gmail.com>
2022-08-08 14:13:38 -0400

    First commit generics

--- interfaces3/src/application/Program.java
+++ interfaces3/src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import services.CanadaInterestService;
 import services.InterestService;
+import services.UsaInterestService;
 
 import java.util.Locale;
 import java.util.Scanner;
@@ -19,10 +20,19 @@ public class Program {
         int months = sc.nextInt();
 
         InterestService cis = new CanadaInterestService(2.0);
-        double payment = cis.payment(amount, months);
+        double cadPayment = cis.payment(amount, months);
 
+        InterestService uis = new UsaInterestService(1.0);
+        double usaPayment = uis.payment(amount, months);
+
+        System.out.println("***CANADIAN INTEREST***");
         System.out.println("Payment after " + months + " months:");
-        System.out.println(String.format("%.2f", payment));
+        System.out.println(String.format("%.2f", cadPayment));
+
+        System.out.println();
+        System.out.println("***USA INTEREST***");
+        System.out.println("Payment after " + months + " months:");
+        System.out.println(String.format("%.2f", usaPayment));
 
         sc.close();
     }
